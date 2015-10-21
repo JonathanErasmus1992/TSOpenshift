39032424e344032fabec13ffae8b52df3417adaf
JonathanErasmus1992 <jonathanerasmus1992@gmail.com>
2015-10-21 13:56:02 +0200

    Fixed Bugs

--- src/main/java/toystore/service/AddInvoiceService.java
+++ src/main/java/toystore/service/AddInvoiceService.java
@@ -3,11 +3,13 @@ package toystore.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import toystore.conf.InvoiceFactory;
 import toystore.domain.Customer;
 import toystore.domain.Invoice;
+import toystore.domain.Orderline;
 import toystore.domain.Orders;
 import toystore.repository.CustomerRepository;
 import toystore.repository.InvoiceRepository;
@@ -27,6 +29,8 @@ public class AddInvoiceService implements AddInvoiceDetails{
     private Orders order;
     private Customer customer;
     private Invoice invoice;
+    private List<Orderline> orderlines;
+    private List<Orderline> orderlinesblank;
     @Override
     public Long addInvoice(Long customerID, Long orderID)
     {
@@ -37,19 +41,35 @@ public class AddInvoiceService implements AddInvoiceDetails{
         order = orderRepository.findOne(orderID);
         if(order==null || order.getCheckout())
             return null;
+        orderlinesblank = new ArrayList<Orderline>();
+        orderlines = order.getOrderlines();
+        List<Orderline> orderlinesi = new ArrayList<Orderline>();
+        for(Orderline orderlineit: orderlines)
+            orderlinesi.add(orderlineit);
 
-        invoice = InvoiceFactory.createInvoice(orderID, order.getTotalPrice(), order.getOrderlines());
+        invoice = InvoiceFactory.createInvoice(orderID, order.getTotalPrice(), orderlinesblank);
         invoiceRepository.save(invoice);
+
         id = invoice.getID();
+
+        invoice = invoiceRepository.findOne(id);
+
+        invoice = new Invoice
+                .Builder(invoice.getOrderID())
+                .copy(invoice)
+                .orderlines(orderlinesi)
+                .build();
+
+        invoiceRepository.save(invoice);
 
         List<Invoice> invoices = customer.getInvoices();
         invoices.add(invoice);
 
         customer = new Customer
-                        .Builder(customer.getUserName())
-                        .copy(customer)
-                        .invoices(invoices)
-                        .build();
+                .Builder(customer.getUserName())
+                .copy(customer)
+                .invoices(invoices)
+                .build();
 
         customerRepository.save(customer);
 
--- src/main/java/toystore/service/EmptyOrderService.java
+++ src/main/java/toystore/service/EmptyOrderService.java
@@ -3,6 +3,7 @@ package toystore.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -23,18 +24,20 @@ public class EmptyOrderService implements EmptyOrderDetails{
     @Override
     public boolean emptyOrder(Long id)
     {
+        orderlines = new ArrayList<Orderline>();
         orders = orderRepository.findOne(id);
         if(orders.getCheckout())
             return false;
         for(Orderline orderline: orders.getOrderlines())
             orderlineRepository.delete(orderline);
         orders = new Orders
-                    .Builder(orders.getDateModified())
-                    .copy(orders)
-                    .dateModified(new Date())
-                    .totalPrice(0)
-                    .orderlines(orderlines)
-                    .build();
+                .Builder(orders.getDateModified())
+                .copy(orders)
+                .dateModified(new Date())
+                .totalPrice(0)
+                .orderlines(orderlines)
+                .build();
+        orderRepository.save(orders);
         return true;
     }
 }
